16f764b2376d775d381f80dd5bda3cfb9b91ac2e
RosettaHalt <rosetta@lugn-design.com>
2012-06-12 21:18:17 +0900

    Load sound effect.

--- js/resource.js
+++ js/resource.js
@@ -1,5 +1,8 @@
 // リソースの読み込み
 tm.preload(function() {
+    tm.sound.SoundManager.add("decide", "sound/se/decide.wav");
+    tm.sound.SoundManager.add("touch", "sound/se/touch.wav");
+    tm.sound.SoundManager.add("clear", "sound/se/clear.wav");
 });
 
 var REVERSE_WAVE_IMAGE = (function(){
