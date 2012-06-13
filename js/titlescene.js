e3081214d79f0eeb7158227a21ec93d6bec6c024
RosettaHalt <rosetta@lugn-design.com>
2012-06-13 21:01:13 +0900

    Add title wave.

--- js/titlescene.js
+++ js/titlescene.js
@@ -11,18 +11,22 @@
         init: function(){
             this.superInit();
             
-            var label = tm.app.Label(32,32);
-            label.x = app.width/2;
-            label.y = app.height/2;
-            label.text = "Chain Wave";
-            label.width = app.width
-            this.addChild(label);
+            this.label = tm.app.Label(32,32);
+            this.label.x = app.width/2;
+            this.label.y = app.height/2;
+            this.label.text = "Chain Wave";
+            this.label.width = app.width
+            this.addChild(this.label);
         },
     
         update: function(){
             if( app.pointing.getPointingEnd() == true ){
+                var wave = Wave(this.label.x*1.5, this.label.y, 1500, 512, TITLE_WAVE_IMAGE);
+                wave.plusScale = 0.02;
+                this.addChild(wave);
+
                 this.addChild( tm.fade.FadeOut(
-                    app.width, app.height, "#000", 1000, function() {
+                    app.width, app.height, "#000", 3000, function() {
                         app.replaceScene(MainScene());
                     })
                 );
